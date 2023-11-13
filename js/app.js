9db6761bdbe8130f65de7e42e5d07626739ab242
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2023-11-13 11:38:14 -0300

    começando lógica do sorteio

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 let listaAmigos = [];
+let sorteioDarPresente = [];
+let sorteioRecebePresente = [];
 
 function adicionar() {
     let nome = document.getElementById('nome-amigo');
@@ -6,6 +8,8 @@ function adicionar() {
 
     if (nome.value.length == 0) {
         window.alert('Informe um nome.')
+    } else if (listaAmigos.indexOf(nome.value) != -1 ) {
+        window.alert('Nome já adicionado. Insira outro nome.');
     } else {
         listaAmigos.push(nome.value)
         console.log(listaAmigos);
@@ -23,3 +27,25 @@ function adicionar() {
     
 }
 
+function sortear() {
+    if (listaAmigos.length <= 1) {
+        window.alert('Informe pelo menos 2 nomes.');
+    } else {
+        console.log('sorteio');
+        let validacao = 1;
+        /*for (let i = 0; i <= listaAmigos.length; i++) {
+            while (validacao == 1){
+                sorteioDarPresente[i] = parseInt(Math.random() * listaAmigos.length);
+
+            }
+            let numeroSorteio = parseInt(Math.random() * listaAmigos.length);
+            console.log(numeroSorteio);
+
+
+        }*/
+    }
+}
+
+function reiniciar() {
+
+}
