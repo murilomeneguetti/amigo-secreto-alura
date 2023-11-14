e3d5cbf0c51e0ceef6030873a7c68b93466b8815
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2023-11-14 14:50:28 -0300

    incluindo função embaralha

--- js/app_alura.js
+++ js/app_alura.js
@@ -1,11 +1,35 @@
+let amigos = [];
+
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
     let lista = document.getElementById('lista-amigos');
-
+    amigos.push(amigo.value);
     if (lista.textContent == '') {
         lista.textContent = amigo.value;
     } else {
         lista.textContent = lista.textContent + ', ' + amigo.value;
     }
     amigo.value = '';
+}
+
+function sortear() {
+    embaralha(amigos);
+    
+}
+
+//função copiada do artigo em: http://cangaceirojavascript.com.br/como-embaralhar-arrays-algoritmo-fisher-yates/
+//algoritmo Fisher–Yates
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        
+        // guarda de um índice aleatório da lista
+        const elemento = lista[indice - 1];
+        
+        lista[indice - 1] = lista[indiceAleatorio];
+        
+        lista[indiceAleatorio] = elemento;
+    }
 }
