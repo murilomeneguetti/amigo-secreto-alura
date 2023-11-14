8ebd2bd2a280e680ea5e60de04b199db4636e066
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2023-11-14 15:21:36 -0300

    continuando js curso

--- js/app_alura.js
+++ js/app_alura.js
@@ -16,8 +16,12 @@ function sortear() {
     embaralha(amigos);
     let sorteio = document.getElementById('lista-sorteio');
 
-    for (let i = 0; i < amigos.length, i++) {
-        sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' -->' + amigos[i+1] + '<br>';
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' -->' + amigos[0] + '<br>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' -->' + amigos[i+1] + '<br>';
+        }
     }
 }
 
@@ -36,4 +40,10 @@ function embaralha(lista) {
         
         lista[indiceAleatorio] = elemento;
     }
+}
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
 }
