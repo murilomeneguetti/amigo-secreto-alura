15e434802ad0d55bce46f87193dabb242ea9d509
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2023-11-14 11:07:16 -0300

    funções para sorteios

--- js/app.js
+++ js/app.js
@@ -27,22 +27,45 @@ function adicionar() {
     
 }
 
+function gerarNumeroAleatorioDar() {
+    let numeroSorteado = parseInt(Math.random() * listaAmigos.length);
+
+    //verificando se o número ja está na lista ou não
+    if (sorteioDarPresente.includes(numeroSorteado)) {
+        gerarNumeroAleatorioDar();
+    } else {
+        sorteioDarPresente.push(numeroSorteado);
+        console.log(sorteioDarPresente);
+    }
+}
+
+function gerarNumeroAleatorioReceber(index) {
+    let numeroSorteado = parseInt(Math.random() * listaAmigos.length);
+
+    //verificando se o número ja está na lista ou não
+    if (sorteioRecebePresente.includes(numeroSorteado) || sorteioDarPresente[index] == numeroSorteado) {
+        gerarNumeroAleatorioReceber();
+    } else {
+        sorteioRecebePresente.push(numeroSorteado);
+        console.log(sorteioRecebePresente);
+    }
+}
+
 function sortear() {
     if (listaAmigos.length <= 1) {
         window.alert('Informe pelo menos 2 nomes.');
     } else {
         console.log('sorteio');
-        console.log('numero de amigos: ' + listaAmigos.length);
+        /*console.log('numero de amigos: ' + listaAmigos.length);
         console.log('sorteioDarPresente.length: ' + sorteioDarPresente.length);
         console.log('sorteioDarPresente.includes(sorteioDarPresente[i]): ' + sorteioDarPresente.includes(sorteioDarPresente[0]));
-        console.log('primeiro amigo: ' + listaAmigos[0]);
-        //let validacao = 1;
+        console.log('primeiro amigo: ' + listaAmigos[0]);*/
         //sorteio não está funcionando ainda, precisa continuar
-        for (let i = 0; i <= listaAmigos.length; i++) {
-            while (sorteioDarPresente.length < listaAmigos.length && sorteioDarPresente.includes(sorteioDarPresente[i])){
-                sorteioDarPresente[i] = parseInt(Math.random() * listaAmigos.length);
-                console.log(`número ${i}: ${sorteioDarPresente[i]}`);
-            }
+        for (let i = 0; i < listaAmigos.length; i++) {
+            gerarNumeroAleatorioDar();
+            console.log(`sorteioDarPresente[${i}]: ` + sorteioDarPresente[i] + ' ' + listaAmigos[sorteioDarPresente[i]]);
+            gerarNumeroAleatorioReceber(i);
+            console.log(`sorteioRecebePresente[${i}]: ` + sorteioRecebePresente[i] + ' ' + listaAmigos[sorteioRecebePresente[i]]);
 
         }
     }
